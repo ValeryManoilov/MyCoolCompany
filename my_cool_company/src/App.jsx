9f80fb4513ab458b328ae76e73f477fb5591e0ee
ValeryManoilov <manojlovv@inbox.ru>
2025-10-02 19:19:27 +0300

    minor changes

--- my_cool_company/src/App.jsx
+++ my_cool_company/src/App.jsx
@@ -1,14 +1,14 @@
 import LeadPage from "./pages/Leadpage.jsx";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, HashRouter} from 'react-router-dom';
 import styles from "./App.module.scss";
 
 function App() {
   return (
-      <BrowserRouter basename="/MyCoolCompany">
+      <HashRouter basename="/MyCoolCompany">
           <Routes>
               <Route path="/" element={<LeadPage/>} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   )
 }
 
